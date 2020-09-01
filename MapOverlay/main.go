b3ee969cd4a930c475bb321f0e1695b1a95d9f53
Zhou Fang <fang2hou@outlook.com>
2020-09-01 23:50:45 +0900

    add a script for parse data mining

---
+++ MapOverlay/main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+type overlay struct {
+	ID                int
+	UIMapArtID        int
+	TextureWidth      int
+	TextureHeight     int
+	OffsetX           int
+	OffsetY           int
+	HitRectTop        int
+	HitRectBottom     int
+	HitRectLeft       int
+	HitRectRight      int
+	PlayerConditionID int
+	Flags             int
+	AreaID            [4]int
+}
+
+type overlayTile struct {
+	ID                int
+	RowIndex          int
+	ColIndex          int
+	LayerIndex        int
+	FileDataID        int
+	WorldMapOverlayID int
+}
+
+func main() {
+	overlaysFileName := "worldmapoverlay.csv"
+	overlayTilesFileName := "worldmapoverlaytile.csv"
+
+	overlays := loadOverlay(overlaysFileName)
+	overlayTiles := loadOverlayTiles(overlayTilesFileName)
+
+	mapTexture := make(map[int]map[string]string)
+
+	for _, overlayTile := range overlayTiles {
+		worldMapOverlayID := overlayTile.WorldMapOverlayID
+		overlay := overlays[worldMapOverlayID]
+
+		textureInfo := fmt.Sprintf("%d:%d:%d:%d",
+			overlay.TextureWidth,
+			overlay.TextureHeight,
+			overlay.OffsetX,
+			overlay.OffsetY)
+
+		thisMap := mapTexture[overlay.UIMapArtID]
+
+		if thisMap == nil {
+			thisMap = make(map[string]string)
+		}
+
+		if thisMap[textureInfo] != "" {
+			thisMap[textureInfo] = thisMap[textureInfo] + ", " + strconv.Itoa(overlayTile.ID)
+		} else {
+			thisMap[textureInfo] = strconv.Itoa(overlayTile.FileDataID)
+		}
+
+		mapTexture[overlay.UIMapArtID] = thisMap
+	}
+
+	exportData("data.lua", mapTexture)
+}
+
+func toInt(text string) int {
+	number, err := strconv.Atoi(text)
+	if err != nil {
+		log.Fatalln("The text cannot be converted to int", err)
+	}
+	return number
+}
+
+func loadOverlay(filePath string) map[int]overlay {
+	csvfile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	defer csvfile.Close()
+
+	csvReader := csv.NewReader(csvfile)
+
+	overlays := make(map[int]overlay)
+
+	// Skip the first line
+	csvReader.Read()
+
+	for {
+		row, err := csvReader.Read()
+		if err != nil && err != io.EOF {
+			log.Fatalf("can not read, err is %+v", err)
+		}
+		if err == io.EOF {
+			break
+		}
+
+		overlays[toInt(row[0])] = overlay{
+			ID:                toInt(row[0]),
+			UIMapArtID:        toInt(row[1]),
+			TextureWidth:      toInt(row[2]),
+			TextureHeight:     toInt(row[3]),
+			OffsetX:           toInt(row[4]),
+			OffsetY:           toInt(row[5]),
+			HitRectTop:        toInt(row[6]),
+			HitRectBottom:     toInt(row[7]),
+			HitRectLeft:       toInt(row[8]),
+			HitRectRight:      toInt(row[9]),
+			PlayerConditionID: toInt(row[10]),
+			Flags:             toInt(row[11]),
+			AreaID: [4]int{
+				toInt(row[12]),
+				toInt(row[13]),
+				toInt(row[14]),
+				toInt(row[15]),
+			},
+		}
+	}
+
+	return overlays
+}
+
+func loadOverlayTiles(filePath string) map[int]overlayTile {
+	csvfile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	defer csvfile.Close()
+
+	csvReader := csv.NewReader(csvfile)
+
+	overlayTiles := make(map[int]overlayTile)
+
+	// Skip the first line
+	csvReader.Read()
+
+	for {
+		row, err := csvReader.Read()
+		if err != nil && err != io.EOF {
+			log.Fatalf("can not read, err is %+v", err)
+		}
+		if err == io.EOF {
+			break
+		}
+
+		overlayTiles[toInt(row[0])] = overlayTile{
+			ID:                toInt(row[0]),
+			RowIndex:          toInt(row[1]),
+			ColIndex:          toInt(row[2]),
+			LayerIndex:        toInt(row[3]),
+			FileDataID:        toInt(row[4]),
+			WorldMapOverlayID: toInt(row[5]),
+		}
+	}
+
+	return overlayTiles
+}
+
+func exportData(filePath string, data map[int]map[string]string) {
+	os.Remove(filePath)
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalln("Couldn't open the result file.", err)
+	}
+	defer file.Close()
+
+	var idList []int
+	for id := range data {
+		idList = append(idList, id)
+	}
+	sort.Ints(idList)
+
+	fmt.Fprintln(file, "local RevealDatabase = {")
+
+	for _, id := range idList {
+		fmt.Fprintln(file, "    ["+strconv.Itoa(id)+"] = {")
+
+		var mapInfoList []string
+		for mapInfo := range data[id] {
+			mapInfoList = append(mapInfoList, mapInfo)
+		}
+		sort.Strings(mapInfoList)
+
+		for _, mapInfo := range mapInfoList {
+			fmt.Fprintf(file, "        [\"%s\"] = \"%s\",\n", mapInfo, data[id][mapInfo])
+		}
+
+		fmt.Fprintln(file, "    },")
+	}
+
+	fmt.Fprintln(file, "}")
+}
