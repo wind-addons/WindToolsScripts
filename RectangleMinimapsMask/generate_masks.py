7dfc62ae5622f26c269965e9f544451a3fb56f76
Zhou Fang <fang2hou@outlook.com>
2020-10-16 11:50:55 +0900

    Update masks

--- RectangleMinimapsMask/generate_masks.py
+++ RectangleMinimapsMask/generate_masks.py
@@ -1,8 +1,8 @@
 from PIL import Image, ImageDraw, ImageFont
 
-for i in range(0, 99):
-    image = Image.new(mode='RGBA', size=(200, 200), color=(255, 255, 255, 255))
+for i in range(0, 127):
+    image = Image.new(mode='RGBA', size=(256, 256), color=(255, 255, 255, 255))
     draw_table = ImageDraw.Draw(im=image)
-    draw_table.rectangle([0, 0, 200, i], (255, 255, 255, 0))
-    draw_table.rectangle([0, 200-i, 200, 200], (255, 255, 255, 0))
-    image.save('masks/{}.png'.format(100-i), 'PNG')
+    draw_table.rectangle([0, 0, 256, i], (255, 255, 255, 0))
+    draw_table.rectangle([0, 256-i, 256, 256], (255, 255, 255, 0))
+    image.save('masks/{}.png'.format(128-i), 'PNG')
